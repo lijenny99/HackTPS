53f57f66131ae047ba65323eb5b5a98fa6e388f1
Jenny Li <lijenny1999@gmail.com>
2018-06-09 19:10:20 -0400

    styling

--- frontend/components/Message.js
+++ frontend/components/Message.js
@@ -2,14 +2,27 @@ import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
 const Message = (props) => (
-    <View style={styles.view}>
-        <Text>{props.text}</Text>
+    <View style={styles.rectangle}>
+        <Text style={styles.txt}>{props.text}</Text>
     </View>
 )
 
 const styles = StyleSheet.create({
-    view: {
-        width: 400,
+    txt: {
+      width: 170,
+      fontSize: 14,
+      textAlign: 'left'
+
+    },
+
+    rectangle: {
+      marginLeft: 40,
+      width: 200,
+      backgroundColor: '#F2F2F2',
+      padding: 15,
+      borderRadius: 15,
+      borderBottomLeftRadius: 0,
+      marginBottom: 10
     }
 })
 
