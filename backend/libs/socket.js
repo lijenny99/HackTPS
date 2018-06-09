afc7b6d0a3e962a76363b67f073eb75d19c94d59
David Zhao <bravetheheat1998@gmail.com>
2018-06-09 16:09:19 -0400

    Auto stash before merge of "master" and "origin/master"

--- backend/libs/socket.js
+++ backend/libs/socket.js
@@ -20,13 +20,14 @@ io.on("connect", socket => {
         }
         socket.join(session_id, () => {
             console.log(`Room with session id ${session_id} with client ${client_id} created`);
-            socket.to(client_id).emit('session_created', {
+            let response = {
                 status: true,
                 client_id: client_id,
                 user_id: data.user_id,
                 time_stamp: time_stamp,
                 session_id: session_id
-            })
+            };
+            io.to(client_id).emit('session_created', response);
         })
 
 
@@ -93,11 +94,7 @@ io.on("connect", socket => {
 });
 
 function gen_session() {
-    let token;
-    crypto.randomBytes(48, (err, buffer) => {
-        token = buffer.toString('hex');
-    })
-    return token
+    return crypto.randomBytes(32).toString("hex");
 }
 
 
