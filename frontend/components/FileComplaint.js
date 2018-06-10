8576e62ace875a343e6407a84107e7c556c80432
Katie Liu <katie_acting@live.ca>
2018-06-09 20:34:52 -0400

    set up file complaint page

--- frontend/components/FileComplaint.js
+++ frontend/components/FileComplaint.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View, TextInput, TouchableHighlight, ImageBackground } from 'react-native';
 
-const FileComplaint = () => (
-    <View>
-        <Text>This is the file complaint page!</Text>
-    </View>
-)
+export default class FileComplaint extends React.Component {
+    static navigationOptions = {
+        title: 'File A Complaint'
+    }
 
-export default FileComplaint;
+    render() {
+        return (
+            <View>
+                <Text>This is the file complaint page!</Text>
+            </View>
+        )
+    }
+} 
