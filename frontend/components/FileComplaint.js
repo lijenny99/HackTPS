61a6216dbb7f2c4572075107c78ccfc7058971ca
Jenny Li <lijenny1999@gmail.com>
2018-06-10 09:57:47 -0400

    file complaint outline

--- frontend/components/FileComplaint.js
+++ frontend/components/FileComplaint.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableHighlight, ImageBackground } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableHighlight, ImageBackground, Picker, ScrollView } from 'react-native';
 
 export default class FileComplaint extends React.Component {
     static navigationOptions = {
@@ -8,9 +8,57 @@ export default class FileComplaint extends React.Component {
 
     render() {
         return (
-            <View>
-                <Text>File A Report</Text>
-            </View>
+            <ScrollView>
+                <Text>Name: </Text>
+                <TextInput style={styles.input} onChange={this.handleText} />
+                <Text>Date/Time Filed: </Text>
+                <Text>Event Location: </Text>
+                <Text>Event Category: </Text>
+                <Picker>
+                  <Picker.Item label="loud" value="Loud Noise"/>
+                  <Picker.Item label="suspicious" value="Suspicious Activity"/>
+                  <Picker.Item label="animal" value="Wild Animal"/>
+
+                </Picker>
+                <Text>Event Duration: </Text>
+                <TextInput style={styles.input} onChange={this.handleText} />
+                <Text>Event Details: </Text>
+                <TextInput style={styles.inputlong} onChange={this.handleText} />
+
+                <Text>Actions Taken: </Text>
+                <TextInput style={styles.inputlong} onChange={this.handleText} />
+                <Text>Impact on individuals/others: </Text>
+                <TextInput style={styles.inputlong} onChange={this.handleText} />
+
+
+
+            </ScrollView>
+
         )
     }
-} 
+}
+
+const styles = StyleSheet.create({
+
+      input: {
+        width: 300,
+        padding: 10,
+        borderWidth: 0.5,
+        borderColor: 'grey',
+        marginBottom: '5%',
+        backgroundColor: '#fcfdff'
+      },
+
+      inputlong: {
+        flex:1,
+        flexWrap: 'wrap',
+        width: 300,
+        height: 200,
+        padding: 10,
+        borderWidth: 0.5,
+        borderColor: 'grey',
+        marginBottom: '5%',
+        backgroundColor: '#fcfdff'
+      },
+
+})
